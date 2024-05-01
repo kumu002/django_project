51469baf6ed7480d2f3c2f077c1a08674b0d69b3
kumu002 <21201041@uap-bd.edu>
2024-05-01 19:54:10 +0600

    5th Commit Kumu

--- GaragekoiProject/urls.py
+++ GaragekoiProject/urls.py
@@ -24,8 +24,8 @@ from registration import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-    path('login/', views.user_login, name='login'),
-    path('signup/', views.user_signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
 ]
 
--- registration/models.py
+++ registration/models.py
@@ -9,27 +9,34 @@ class Users(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
-    phone_number = models.IntegerField()
+    phone_number = models.IntegerField(max_length=11, null=True, blank=True)
     gender = models.CharField(max_length=255, null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
     image = models.ImageField(upload_to='static/images', null=True, blank=True)
     is_active = models.BooleanField(default=True)
     role = models.ForeignKey(User_roles, on_delete=models.CASCADE)
 
+
 class Location(models.Model):
-    loc_name=models.CharField(max_length=200)
+    loc_name = models.CharField(max_length=200)
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+
 class Parking_places(models.Model):
     car_slot = models.IntegerField()
     bike_slot = models.IntegerField()
     satuts = models.BooleanField(default=True)
     role = models.ForeignKey(User_roles, on_delete=models.CASCADE)
+
+
 class Payment(models.Model):
-    pay_amount=models.IntegerField()
+    pay_amount = models.IntegerField()
+
+
 class Rent(models.Model):
     amount = models.IntegerField()
-    start_time=models.DateTimeField()
-    end_time=models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     role = models.ForeignKey(User_roles, on_delete=models.CASCADE)
     parking = models.ForeignKey(Parking_places, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
